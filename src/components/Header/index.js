867a66b5cd005849b306980fb6f2d4fe2146ac8f
ideasjaed <ideas.jaed@gmail.com>
2023-04-20 17:54:27 -0600

    add header component and views

---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './style.scss'
+
+const index = () => {
+  return (
+    <header>
+      <h1>HACKER NEWS</h1>
+    </header>
+  )
+}
+
+export default index
---
+++ src/views/Home/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './style.scss';
+import Layout from '../Layout';
+
+const index = () => {
+  return (
+    <Layout>home</Layout>
+  )
+}
+
+export default index
